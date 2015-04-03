6004f75215e5c56d7645b4415e2bfd300235c54f
Shannon Burns <kari.shay@gmail.com>
2015-04-03 13:15:12 -0700

    added templating, index route and cleaned up api routes

--- web.py
+++ web.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+###========= Teacher Interface =========###
+
+@app.route('/')
+def index():
+  return render_template("index.html")
+
+
+###=========== API endpoints ===========###
+
+
 @app.route('/teachers')
 def teachers_json():
   return render_template("teachers.json")
@@ -8,6 +18,11 @@ def teachers_json():
 @app.route('/activity')
 def activity_json():
   return render_template("activity.json")
+
+
+###=====================================###
+###=====================================###
+
 
 if __name__ == "__main__":
   app.run()
